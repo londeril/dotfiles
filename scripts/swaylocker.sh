5564ab22518b9dfb267eb6cc656f0dc27d78b312
Daniel <daniel@dashwood.ch>
2023-09-03 21:13:43 +0200

    lockscreen config

--- scripts/swaylocker.sh
+++ scripts/swaylocker.sh
@@ -3,15 +3,21 @@
 swaylock \
 	--screenshots \
 	--clock \
+	--timestr %H:%M \
+	--datestr "%d. %B %Y" \
 	--indicator \
 	--indicator-radius 100 \
 	--indicator-thickness 7 \
 	--effect-blur 7x5 \
 	--effect-vignette 0.5:0.5 \
-	--ring-color bb00cc \
+	--ring-color 0d73ccff \
 	--key-hl-color 880033 \
 	--line-color 00000000 \
 	--inside-color 00000088 \
 	--separator-color 00000000 \
 	--grace 2 \
-	--fade-in 0.2
+	--fade-in 0.2 \
+	--font "Iosevka Nerd Font" \
+	--font-size 36 \
+	--text-color a0b3c5ff
+
