af0fc22ab69e779840aaa55605b4405d3b783c0a
Daniel Müller <daniel@dashwood.ch>
2024-02-02 15:53:15 +0100

    screensaver

--- scripts/screensaver.sh
+++ scripts/screensaver.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # this script runs feh in fullscreen mode to display screensaverpics on both displays at the office
-feh --fullscreen --slideshow-delay 7 --auto-zoom --hide-pointer --xinerama-index 1 /home/daniel/Pictures/ScreensaverPics &
+feh --fullscreen --slideshow-delay 7 --auto-zoom --hide-pointer --xinerama-index 0 /home/daniel/Pictures/ScreensaverPics &
