7aac17f9fca4113b1acebf5b8cb655952dc5840a
Daniel Müller <daniel@dashwood.ch>
2024-02-02 16:30:13 +0100

    screensaver

--- scripts/screensaver.sh
+++ scripts/screensaver.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 # this script runs feh in fullscreen mode to display screensaverpics on both displays at the office
-feh --fullscreen --slideshow-delay 7 --auto-zoom --hide-pointer --xinerama-index 0 --randomize /home/daniel/Pictures/ScreensaverPics &
+
+# let's check on which host we are running
+HOSTNAME=`hostname`
+
+if [[ $HOSTNAME == "Nova" ]]; then
+	feh --fullscreen --slideshow-delay 7 --auto-zoom --hide-pointer --xinerama-index 0 --randomize /home/daniel/Pictures/ScreensaverPics &
+elif [[ $HOSTNAME == "viper" ]]; then
+	feh --fullscreen --slideshow-delay 7 --auto-zoom --hide-pointer --xinerama-index 1 --randomize /home/daniel/Pictures/ScreensaverPics &
+else 
+	# I don't know on which host we are running... better do nothing
+	echo "no hostname found"
+fi
+
+
