8d7b35f9effde23b4e895223641f4af1c792cd1e
Daniel <daniel@dashwood.ch>
2024-02-03 23:12:02 +0100

    stuff

--- scripts/swayidle.sh
+++ scripts/swayidle.sh
@@ -13,8 +13,11 @@ OFFICESCREEN=300
 swayidle -w \
                 timeout $BATDIM '/home/daniel/.dotfiles/scripts/idle.sh BATDIM' resume '/home/daniel/.dotfiles/scripts/idle.sh BATUNDIM' \
                 timeout $BATSCREEN '/home/daniel/.dotfiles/scripts/idle.sh BATSCREEN' \
-                timeout $BATLOCK '/home/daniel/.dotfiles/scripts/idle.sh BATLOCK' \
                 timeout $BATSUSPEND '/home/daniel/.dotfiles/scripts/idle.sh BATSUSPEND' \
                 timeout $SCREENSAVER '/home/daniel/.dotfiles/scripts/idle.sh SCREENSAVER' resume '/home/daniel/.dotfiles/scripts/idle.sh KILLSAVER' \
-                timeout $ACLOCK '/home/daniel/.dotfiles/scripts/idle.sh ACLOCK' \
                 timeout $OFFICESCREEN '/home/daniel/.dotfiles/scripts/idle.sh OFFICESCREEN' 
+
+
+
+# timeout $BATLOCK '/home/daniel/.dotfiles/scripts/idle.sh BATLOCK' \
+# timeout $ACLOCK '/home/daniel/.dotfiles/scripts/idle.sh ACLOCK' \
