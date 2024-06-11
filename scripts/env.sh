c4489771b3b38f5f9286f20cbc782306aa749b56
Daniel <daniel@dashwood.ch>
2024-06-11 07:56:05 +0200

    new env

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,4 +1,4 @@
 # setting environment variables and stuff for GTK/QT Apps in non-gnome / non-kde sessions
-dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP QT_QPA_PLATFORMTHEME GTK_THEME HYPRLAND_INSTANCE_SIGNATURE 
+dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP QT_QPA_PLATFORMTHEME GTK_THEME HYPRLAND_INSTANCE_SIGNATURE
 systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP QT_QPA_PLATFORMTHEME GTK_THEME HYPRLAND_INSTANCE_SIGNATURE
-
+gsettings set org.gnome.desktop.interface icon-theme 'Newaita-reborn-dark'
