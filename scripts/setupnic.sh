8dc233f0d1f71c179d751d42915cb5d2155a2160
Daniel Müller <daniel@dashwood.ch>
2024-08-28 14:05:25 +0200

    setupnic

--- scripts/setupnic.sh
+++ scripts/setupnic.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 # this script will bring the NetworkManager Connection "Setup NIC Manual" down and up again - usefull after setting a new IPv4 address on the connection
-ConnectionName="Setup NIC Manual"
+
+case $1 in
+	viper )
+		ConnectionName="Setup NIC Manual"
+		;;
+	nova )
+		ConnectionName="USB NIC - Manual"
+esac
+
 nmcli connection down "$ConnectionName"
 sleep 2
 nmcli connection up "$ConnectionName"
