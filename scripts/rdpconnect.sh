94e05d5f9d3da2c781534d1fe923a0bbf49d58f7
Daniel Müller <daniel@dashwood.ch>
2024-09-05 13:25:38 +0200

    rdp

--- scripts/rdpconnect.sh
+++ scripts/rdpconnect.sh
@@ -1,7 +1,21 @@
-!#/bin/bash
-SERVER="192.168.73.20"
-USERNAME="sysmin"
-DOMAIN="localhost"
-PASSWORD=$(secret-tool lookup duresco-vbr1 password)
+#!/bin/bash
+# this script will connect to RDP Servers using the supplied args
+# $1 = Servername
+# $2 = IP Address
+# $3 = Username
+# $4 = Domain
 
-xfreerdp3 /v:$SERVER /u:$USERNAME /d:$DOMAIN /p:"$PASSWORD" +dynamic-resolution /drive:RDPShare,/home/daniel/RDPShare +clipboard
+
+SERVERNAME=$1
+IP=$2
+USERNAME=$3
+DOMAIN=$4
+PASSWORD=$(secret-tool lookup $SERVERNAME password)
+
+
+#SERVERNAME="duresco-vbr1"
+#IP="192.168.73.20"
+#USERNAME="sysmin"
+#DOMAIN="localhost"
+
+xfreerdp3 /v:$IP /u:$USERNAME /d:$DOMAIN /p:"$PASSWORD" +dynamic-resolution /drive:RDPShare,/home/daniel/RDPShare +clipboard /cert:ignore
