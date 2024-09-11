874ad50dc2448857fb8e9898d8f06966850aab3e
Daniel Müller <daniel@dashwood.ch>
2024-09-11 07:51:29 +0200

    archinstall update

---
+++ archinstall/umount.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# unmount all mounted disks
+swapoff -a
+umount /mnt/.snapshots
+umount /mnt/home
+umount /mnt/var/cache
+umount /mnt/var/lib/libvirt
+umount /mnt/var/log
+umount /mnt/var/tmp
+umount /mnt/swap
+umount /mnt/boot
