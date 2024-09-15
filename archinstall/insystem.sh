941ed6cf104d072f57a2d1dc2ec71d9e4d2e5654
Daniel Müller <daniel@dashwood.ch>
2024-09-15 19:34:31 +0200

    clipse

--- archinstall/insystem.sh
+++ archinstall/insystem.sh
@@ -71,7 +71,7 @@ sudo pacman -Syu sublime-text
 
 #insync
 echo "getting needed packages from the AUR"
-yay -S 1password pcloud-drive typora nccm-git snapper-support btrfs-assistant hyprshot swayosd-git
+yay -S 1password clipse pcloud-drive typora nccm-git snapper-support btrfs-assistant hyprshot swayosd-git
 
 #echo "deploying nccm wrapper"
 #sudo mv /usr/bin/nccm /usr/bin/nccm-bin
