42860143a8bb1d1723c48449e324a3224eb041ca
Daniel Müller <daniel@dashwood.ch>
2024-10-08 09:03:17 +0200

    stuff

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,4 +1,6 @@
 # setting environment variables and stuff for GTK/QT Apps in non-gnome / non-kde sessions
-dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP QT_QPA_PLATFORMTHEME GTK_THEME HYPRLAND_INSTANCE_SIGNATURE
-systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP QT_QPA_PLATFORMTHEME GTK_THEME HYPRLAND_INSTANCE_SIGNATURE
-gsettings set org.gnome.desktop.interface icon-theme 'Newaita-reborn-dark'
+#dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP QT_QPA_PLATFORMTHEME GTK_THEME HYPRLAND_INSTANCE_SIGNATURE
+dbus-update-activation-environment --systemd WAYLAND_DISPLAY QT_QPA_PLATFORMTHEME GTK_THEME 
+#systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP QT_QPA_PLATFORMTHEME GTK_THEME HYPRLAND_INSTANCE_SIGNATURE
+systemctl --user import-environment WAYLAND_DISPLAY QT_QPA_PLATFORMTHEME GTK_THEME
+
--- scripts/gnome-defaults.sh
+++ scripts/gnome-defaults.sh
@@ -2,3 +2,5 @@
 # this script will set some sane defaults
 
 gsettings set org.gnome.desktop.interface cursor-theme 'volantes_cursors'
+gsettings set org.gnome.desktop.interface icon-theme 'Newaita-reborn-dark'
+
