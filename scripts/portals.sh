344bd2888e6110990845ba8d116dcdeb0f53945a
Daniel Müller <daniel@dashwood.ch>
2024-10-12 12:41:33 +0200

    portals

--- scripts/portals.sh
+++ scripts/portals.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sleep 4
-killall -9 xdg-desktop-portal-wlr
+#killall -9 xdg-desktop-portal-wlr
 killall -9 xdg-desktop-portal
-killall -9 xdg-desktop-portal-kde
+#killall -9 xdg-desktop-portal-kde
 killall -9 xdg-desktop-portal-gtk
 /usr/lib/xdg-desktop-portal-hyprland &
 sleep 4
