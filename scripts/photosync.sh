8fbee571e4868697650b74fc1612cf69ffec72b6
Daniel Müller <daniel@dashwood.ch>
2024-11-05 22:02:17 +0100

    photosync

--- scripts/photosync.sh
+++ scripts/photosync.sh
@@ -10,4 +10,8 @@
 rsync -avz /run/media/daniel/CryptStore/Masters/ daniel@172.16.3.10:/mnt/storage/DashwoodStore/Photos/Masters/ --delete --progress
 
 # mirror CryptStore Darktable Settings to DashwoodStore Darktable Settings
-rsync -avz /run/media/daniel/CryptStore/Darktable/ daniel@172.16.3.10:/mnt/storage/DashwoodStore/Photos/Masters/Darktable/ --delete --progress
+#rsync -avz /run/media/daniel/CryptStore/Darktable/ daniel@172.16.3.10:/mnt/storage/DashwoodStore/Darktable/ --delete --progress
+
+# mirror Darktable Settings to CryptStore and DashwoodStore
+rsync -av /home/daniel/.config/darktable/ /run/media/daniel/CryptStore/Darktable/ --delete --progress
+rsync -avz /home/daniel/.config/darktable/ daniel@172.16.3.10:/mnt/storage/DashwoodStore/Darktable/ --delete --progress
