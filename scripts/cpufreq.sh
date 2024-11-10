671446725c9e9d8c8960b3fe967889a0266d6d10
Daniel Müller <daniel@dashwood.ch>
2024-11-10 21:15:15 +0100

    updates

--- scripts/cpufreq.sh
+++ scripts/cpufreq.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # display a live graph for cpu frequencies
-watch -n0.1 "grep '^[c]pu MHz' /proc/cpuinfo | column"
+#watch -n0.1 "grep '^[c]pu MHz' /proc/cpuinfo | column"
+watch -n0.5 "grep '^[c]pu MHz' /proc/cpuinfo | awk '{if (\$4 >= 4000) printf \"\033[32m%s\033[0m\n\", \$0; else print \$0}' | column -t"
--- scripts/photosync.sh
+++ scripts/photosync.sh
@@ -6,6 +6,12 @@
 # mirror CryptStore Darktable Settings to BigData Darktable Settings
 #rsync -av /run/media/daniel/CryptStore/Darktable/ /mnt/BigData/Darktable/ --delete --progress
 
+# copy local changes to CryptStore
+rsync -av /home/daniel/Pictures/Masters/Kids/2024/ /run/media/daniel/CryptStore/Masters/Kids/2024/ --delete --progress
+rsync -av /home/daniel/Pictures/Masters/Import/ /run/media/daniel/CryptStore/Masters/Import/ --delete --progress
+rsync -av /home/daniel/Pictures/Masters/Trips/Hyeres_2024/ /run/media/daniel/CryptStore/Masters/Trips/Hyeres_2024/ --delete --progress
+rsync -av /home/daniel/Pictures/Masters/Trips/Namibia_2024/ /run/media/daniel/CryptStore/Masters/Trips/Namibia_2024/ --delete --progress
+
 # mirror CryptStore Masters to DashwoodStore Masters
 rsync -avz /run/media/daniel/CryptStore/Masters/ daniel@172.16.3.10:/mnt/storage/DashwoodStore/Photos/Masters/ --delete --progress
 
