7936687a9827b52607e6f4916e201ca8ee5fe3ec
Daniel Müller <daniel@dashwood.ch>
2024-11-15 15:18:59 +0100

    housekeeping

--- scripts/Old/gkey.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-g815-led -a 0080ff
--- scripts/Old/rclone-mounter.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-rclone mount --daemon --vfs-cache-mode full OneDrive: /PATH/TO/MOUNT/POINT
-rclone mount --daemon --vfs-cache-mode full OneDrive-ECM: /PATH/TO/MOUNT/POINT
-rclone mount --daemon --vfs-cache-mode full Ecmacom: /PATH/TO/MOUNT/POINT
-rclone mount --daemon --vfs-cache-mode full Dropbox: /PATH/TO/MOUNT/POINT
