975ece4e36a3d9eb8de50d98eef06e00918877aa
Daniel Müller <daniel@dashwood.ch>
2024-11-17 21:57:40 +0100

    backup

--- scripts/backup_runner.sh
+++ scripts/backup_runner.sh
@@ -2,9 +2,7 @@
 # this script will run a backup of the user folder excluding temparary and cloud data
 case "$1" in
 backup)
-  cd /home/daniel/
-  restic -r sftp:daniel@172.16.3.10:/mnt/storage/DashwoodStore/NovaBackup backup . --password-file="/home/daniel/.NovaBackup" --exclude-file="/home/daniel/.dotfiles/nova-excludes.restic"
-  pkexec restic -r sftp:daniel@172.16.3.10:/mnt/storage/DashwoodStore/NovaBackup backup / --password-file="/home/daniel/.NovaBackup" --exclude-file="/home/daniel/.dotfiles/novasystem-excludes.restic"
+  pkexec restic -r sftp:daniel@172.16.3.10:/mnt/storage/DashwoodStore/NovaBackup backup / --password-file="/home/daniel/.NovaBackup" --exclude-file="/home/daniel/.dotfiles/nova-excludes.restic"
   CURRENTDATE=$(date "+%d. %b. %T")
   echo $CURRENTDATE >/home/daniel/.config/lastest_backup
   exec /home/daniel/.dotfiles/scripts/launch.sh
