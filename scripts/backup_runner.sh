d47e67ac06a7677382b7fe3baedf08268e8dca0a
Daniel Müller <daniel@dashwood.ch>
2024-12-09 20:29:00 +0100

    restic and stuff

--- scripts/backup_runner.sh
+++ scripts/backup_runner.sh
@@ -2,13 +2,13 @@
 # this script will run a backup of the user folder excluding temparary and cloud data
 case "$1" in
 backup)
-  pkexec restic -r sftp:daniel@172.16.3.10:/mnt/storage/DashwoodStore/NovaBackup backup / --password-file="/home/daniel/.NovaBackup" --exclude-file="/home/daniel/.dotfiles/nova-excludes.restic"
+  pkexec restic -r sftp:daniel@172.16.3.10:/mnt/backup/RavenBackup backup / --password-file="/home/daniel/.RavenBackup" --exclude-file="/home/daniel/.dotfiles/raven-excludes.restic"
   CURRENTDATE=$(date "+%d. %b. %T")
   echo $CURRENTDATE >/home/daniel/.config/lastest_backup
   exec /home/daniel/.dotfiles/scripts/launch.sh
   ;;
 snapshots)
-  restic -r sftp:daniel@172.16.3.10:/mnt/storage/DashwoodStore/NovaBackup snapshots --password-file /home/daniel/.NovaBackup
+  restic -r sftp:daniel@172.16.3.10:/mnt/backup/RavenBackup snapshots --password-file /home/daniel/.RavenBackup
   ;;
 *)
   echo "Usage: backup-runner backup|snapshots"
