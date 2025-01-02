a9a735c0e4a5875f90687c6525c2116a5fdfaab7
Daniel Müller <daniel@dashwood.ch>
2025-01-02 22:19:44 +0100

    backup

--- scripts/backup_runner.sh
+++ scripts/backup_runner.sh
@@ -5,7 +5,7 @@ backup)
   pkexec restic -r sftp:daniel@172.16.3.10:/mnt/backup/RavenBackup backup / --password-file="/home/daniel/.RavenBackup" --exclude-file="/home/daniel/.dotfiles/raven-excludes.restic"
   CURRENTDATE=$(date "+%d. %b. %T")
   echo $CURRENTDATE >/home/daniel/.config/latest_backup
-  exec /home/daniel/.dotfiles/scripts/launch.sh
+  #exec /home/daniel/.dotfiles/scripts/launch.sh
   ;;
 snapshots)
   restic -r sftp:daniel@172.16.3.10:/mnt/backup/RavenBackup snapshots --password-file /home/daniel/.RavenBackup
