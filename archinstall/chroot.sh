dd41e5d99bd6ed350881fcb5ac85d484edc8ede2
Daniel Müller <daniel@dashwood.ch>
2025-02-04 15:14:38 +0100

    vconsole font

--- archinstall/chroot.sh
+++ archinstall/chroot.sh
@@ -8,6 +8,8 @@ echo "LANG=en_US.UTF-8" >/etc/locale.conf
 echo "en_US.UTF-8 UTF-8" >>/etc/locale.gen
 locale-gen
 echo "KEYMAP=de_CH-latin1" >/etc/vconsole.conf
+echo "XKBLAYOUT=ch" >>/etc/vconsole.conf
+echo "FONT=sun12x22" >>/etc/vconsole.conf
 
 echo "generating adjtime"
 hwclock --systohc
