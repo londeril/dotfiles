a60ea12fa53482a0c0600bf331e964e6be6c5bf3
Daniel Müller <daniel@dashwood.ch>
2025-02-21 10:14:51 +0100

    added more shares

--- scripts/mounter.sh
+++ scripts/mounter.sh
@@ -2,9 +2,22 @@
 # this script will mount, unmount and check the status of $MOUNT
 
 MOUNT="/mnt/ecm"
+MOUNT1="/mnt/erlenhof/allgemein"
 WG_MANAGER="~/.dotfiles/scripts/wireguard-manager.sh"
 STATUS_MOUNTED_STR='{"text":"Mounted","class":"connected","alt":"mounted"}'
 STATUS_UNMOUNTED_STR='{"text":"Unmounted","class":"disconnected","alt":"unmounted"}'
+
+function local_mount() {
+  mount $MOUNT
+  mount $MOUNT1
+
+}
+
+function local_umount() {
+  umount $MOUNT
+  umount $MOUNT1
+
+}
 
 function status() {
   #check if //int.ecmacom.ch is mounted return the status of the grep command
@@ -15,22 +28,22 @@ function status() {
 function toggle() {
   # check status and act acordingly
   status &&
-    if umount $MOUNT >/dev/null 2>&1; then
+    if local_umount >/dev/null 2>&1; then
       {
-        notify-send -u normal -t 3000 "$MOUNT unmounted"
+        notify-send -u normal -t 3000 "shares unmounted"
       }
     else
       {
-        notify-send -u critical -t 0 "Could not unmount $MOUNT."
+        notify-send -u critical -t 0 "Could not unmount shares."
       }
     fi ||
-    if mount $MOUNT; then
+    if local_mount; then
       {
-        notify-send -u normal -t 3000 "$MOUNT mounted"
+        notify-send -u normal -t 3000 "shares mounted"
       }
     else
       {
-        notify-send -u critical -t 3000 "Could not mount $MOUNT."
+        notify-send -u critical -t 3000 "Could not mount shares."
       }
     fi
 }
