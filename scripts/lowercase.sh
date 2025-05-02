e882b087be2d6368eaaac0032785912d12863fc6
Daniel Müller <daniel@dashwood.ch>
2025-05-02 19:32:38 +0200

    stuff

---
+++ scripts/lowercase.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# convert all file's extentions in the current directory to lowercase
+#
+find . -name '*.*' -exec sh -c '
+  a=$(echo "$0" | sed -r "s/([^.]*)\$/\L\1/");
+  [ "$a" != "$0" ] && mv "$0" "$a" ' {} \;
