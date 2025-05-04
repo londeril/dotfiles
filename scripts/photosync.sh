f00de962618208d703cca26b37eb7fbba37839e6
Daniel Müller <daniel@dashwood.ch>
2025-05-04 07:57:58 +0200

    stuff

--- scripts/photosync.sh
+++ scripts/photosync.sh
@@ -7,10 +7,13 @@
 #rsync -av /run/media/daniel/CryptStore/Darktable/ /mnt/BigData/Darktable/ --delete --progress
 
 # mirror Masters to DashwoodStore Masters
-rsync -avz /home/daniel/Masters/ daniel@172.16.3.10:/mnt/storage/DashwoodStore/Photos/Masters/ --delete --progress
+rsync -av /home/daniel/Masters/ daniel@172.16.3.10:/mnt/storage/DashwoodStore/Photos/Masters/ --delete --progress
+
+# mirror Video RAW to Backup Disk on Arandur
+rsync -av /home/daniel/Videos/RAW_Footage/ daniel@172.16.3.10:/mnt/Backup/RAW_Footage/
 
 # mirror CryptStore Darktable Settings to DashwoodStore Darktable Settings
 #rsync -avz /run/media/daniel/CryptStore/Darktable/ daniel@172.16.3.10:/mnt/storage/DashwoodStore/Darktable/ --delete --progress
 
 # mirror Darktable Settings to CryptStore and DashwoodStore
-rsync -avz /home/daniel/.config/darktable/ daniel@172.16.3.10:/mnt/storage/DashwoodStore/Darktable/ --delete --progress
+rsync -av /home/daniel/.config/darktable/ daniel@172.16.3.10:/mnt/storage/DashwoodStore/Darktable/ --delete --progress
