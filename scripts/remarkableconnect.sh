ce2863f3a352be01d55eb0a372c4b2abfa2ba36e
Daniel Müller <daniel@dashwood.ch>
2025-05-22 21:55:00 +0200

    stuff

---
+++ scripts/remarkableconnect.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# this script will connect to RDP Servers using the supplied args
+
+PASSWORD=$(
+  op item get "Erlenhof Domain" --account "6HT667UGUJC45JLF6VZQCQIDLE" --vault "Private" --format=json | jq -r '.fields[] | select(.type=="CONCEALED").value'
+)
+
+#op item get "$SECRETLOOKUP" --vault $VAULT --fields password --reveal
+
+#secret-tool lookup $SECRETLOOKUP password
+
+#xfreerdp3 /v:$IP /u:$USERNAME /d:$DOMAIN /p:"$PASSWORD" +dynamic-resolution /kbd:layout:0x00000807 /drive:RDPShare,/home/daniel/RDPShare +clipboard /cert:ignore &
+
+xfreerdp3 /home/daniel/Data/rdps/Erlenhof_RemoteApp_reMarkable.rdp /u:d.mueller /d:ERLENHOF01 /p:"$PASSWORD" +dynamic-resolution /kbd:layout:0x00000807 /drive:RDPShare,/home/daniel/RDPShare +clipboard /cert:ignore &
