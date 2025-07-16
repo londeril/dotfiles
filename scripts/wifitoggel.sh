69a721e2f7057f55a3e8ac7762e0c17683173acd
Daniel Müller <daniel@dashwood.ch>
2025-07-16 16:27:25 +0200

    stuff

--- scripts/wifitoggel.sh
+++ scripts/wifitoggel.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 check_status() {
-  status=$(nmcli radio wifi)
-  if [[ $status == "disabled" ]]; then
+  status=$(nmcli -f GENERAL.STATE device show wlp3s0 | awk '{print $2}')
+  #status=$(nmcli radio wifi)
+  if [[ $status == "30" ]]; then
     notify-send "WiFi is off - turning it on"
-    nmcli radio wifi on
+    nmcli device connect wlp3s0
+    #nmcli radio wifi on
   else
     notify-send "WiFi is on - tunring it off"
-    nmcli radio wifi off
+    nmcli device disconnect wlp3s0
+    #nmcli radio wifi off
   fi
 }
 
